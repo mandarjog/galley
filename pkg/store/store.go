f3727dda062b27e96595db98915cc629e1deea76
Mandar U Jog <mjog@google.com>
2017-06-20 10:21:54 -0700

    Made all components interfaces

--- pkg/store/store.go
+++ pkg/store/store.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package shared contains types and functions that are used across the full
-// set of galley commands.
+// Package store contains interfaces and implementations
+// of key value storage layer.
 
 package store
 
@@ -25,49 +25,74 @@ import (
 	"context"
 )
 
+// KeyValue provides a generic interface to a key value store.
+type KeyValue interface {
+	Reader
+	Writer
+	Watcher
+	Locker
+	fmt.Stringer
+	io.Closer
+}
+
+// Reader defines read operations of a key-value store.
+type Reader interface {
+	// Get value at a key, false if not found.
+	Get(key string) (value []byte, revision int64, found bool)
+
+	// List keys with the key prefix. Reply includes values.
+	List(keyPrefix string) (data map[string]string, revision int64, err error)
+}
+
+// Writer defines write operations of a key-value store.
+type Writer interface {
+	// Set a value. revision is used for optimistic concurrency.
+	// To opt out of optimistic concurrency use revision = 0
+	Set(key string, value []byte, revision int64) (out_revision int64, err error)
+
+	// Delete a key.
+	Delete(key string) error
+}
+
+// Watcher defines a wachable store.
+type Watcher interface {
+	// Watch a storage tree rooted at 'key'
+	// Watch can be canceled by the caller by canceling the context.
+	// Watch returns an Event channel that streams changes.
+	// Server closes channel if there is an error.
+	// revision == 0 indicates watch starts with the current revision.
+	// Attempting to start a watch on a revision that is not available (due to compaction)
+	// results in an error.
+	Watch(ctx context.Context, key string, revision int64) (<-chan Event, error)
+}
 type EventType int32
 
 const (
-	Event_PUT    EventType = iota
-	Event_DELETE EventType = 1
+	PUT EventType = iota // ADD + UPDATE
+	DELETE
 )
 
 // Event is a change that has occurred to the underlying store.
 type Event struct {
-	// Type of event ADD+UPDATE  or DELETE
-	Type EventType
-	// Key is the Affected Key.
-	Key string
-	// Value is the Value after the update.
-	Value []byte
-	// Previous Value is the value before the update.
-	Previous_Value []byte
-	// Repository revision at the last update.
-	Revision int64
+	Type          EventType // Type of event ADD+UPDATE  or DELETE
+	Key           string    // Key is the Affected Key.
+	Value         []byte    // Value is the Value after the update.
+	PreviousValue []byte    // Previous Value is the value before the update.
+	Revision      int64     // Repository revision at the last update.
 }
 
-// KeyValueStore provides a generic interface to a key value store.
-type KeyValueStore interface {
-	KVReader
-
-	KVWriter
-
-	Watcher
-
+// Locker defines a lockable store.
+type Locker interface {
 	// Lock obtains a lock (or lease) a tree rooted at key and
 	// returns a LockedKVWriter. The lock is valid for ttl duration.
 	// All write operations fail after the lock expires.
 	// Before validation requests are sent out, Galley acquires a
 	// timebound lock (lease) on the part of the tree in question.
-	Lock(key string, ttl time.Duration) (LockedKVWriter, error)
-
-	fmt.Stringer
-
-	io.Closer
+	Lock(key string, ttl time.Duration) (LockedWriter, error)
 }
 
-type LockedKVWriter interface {
-	KVWriter
+type LockedWriter interface {
+	Writer
 
 	// Renew lease on the lock
 	Renew(ttl time.Duration) error
@@ -75,30 +100,4 @@ type LockedKVWriter interface {
 	// Unlock the writer. All subsequest operations should
 	// fail after unlocking.
 	Unlock() error
-}
-
-// KVWriter defines write operations of KVStore.
-type KVWriter interface {
-	// Set a value.
-	Set(key string, value []byte, revision int64) (out_revision int64, err error)
-
-	// Delete a key.
-	Delete(key string) error
-}
-
-// KVReader defines read operations of KVStore.
-type KVReader interface {
-	// Get value at a key, false if not found.
-	Get(key string) (value []byte, revision int64, found bool)
-
-	// List keys with the prefix.
-	List(key string, recurse bool) (data map[string]string, revision int64, err error)
-}
-
-type Watcher interface {
-	// Watch a storage tree rooted at 'key'
-	// Watch can be canceled by the caller by canceling the context.
-	// Watch returns an Event channel that streams changes.
-	// Server closes channel if there is an error.
-	Watch(ctx context.Context, key string) (<-chan Event, error)
 }
