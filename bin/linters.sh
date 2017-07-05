f4a43bbbf74e429e1f0ee0793f1c9a1549978908
Jun Mukai <jun.mukai@gmail.com>
2017-07-05 10:37:26 -0700

    remove warning messages on bin/linters.sh (#44)
    - use megacheck instead of gosimple/staticcheck/unused
      Same as
      https://github.com/istio/istio/commit/551226382dbf84c509b0f08ece573e32c9dcc3c5#diff-a45186e4c525a7b8a78dcdbef0c30ede,
    - stop using bazel_tools//tools/build_defs/docker:docker.bzl
      since it's deprecated

--- bin/linters.sh
+++ bin/linters.sh
@@ -68,6 +68,7 @@ go_metalinter() {
     if [[ -z ${PKGS[@]} ]]; then
         echo 'No packages set'
     else
+        # updated to avoid WARNING: staticcheck, gosimple, and unused are all set, using megacheck instead
         gometalinter\
           --concurrency=4\
           --enable-gc\
@@ -81,15 +82,13 @@ go_metalinter() {
           --enable=gofmt\
           --enable=goimports\
           --enable=golint --min-confidence=0 --exclude=.pb.go --exclude=pkg/config/proto/combined.go --exclude="should have a package comment"\
-          --enable=gosimple\
           --enable=ineffassign\
           --enable=interfacer\
           --enable=lll --line-length=160\
+          --enable=megacheck \
           --enable=misspell\
-          --enable=staticcheck\
           --enable=structcheck\
           --enable=unconvert\
-          --enable=unused\
           --enable=varcheck\
           --enable=vet\
           --enable=vetshadow\
